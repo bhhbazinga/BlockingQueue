655bc969b961b568532bc17e1fd0569cf5cab937
bhhbazinga <77611355@qq.com>
2019-11-18 13:59:31 -0500

    first commit

---
+++ blocking_queue.h
@@ -0,0 +1,94 @@
+
+#include <atomic>
+#include <condition_variable>
+#include <mutex>
+
+template <typename T>
+class BlockingQueue {
+ public:
+  BlockingQueue() : head_(new Node), tail_(head_), size_(0) {}
+  ~BlockingQueue() {
+    T value;
+    while (TryDequeue(value))
+      ;
+    delete head_;
+  }
+
+  void Enqueue(const T& value) {
+    std::lock_guard<std::mutex> lk(tail_mutex_);
+    tail_->value = value;
+    InternalEnqueue();
+  }
+
+  void Enqueue(T&& value) {
+    std::lock_guard<std::mutex> lk(tail_mutex_);
+    tail_->value = std::move(value);
+    InternalEnqueue();
+    return tail_->value;
+  }
+
+  bool TryDequeue(T& value) {
+    std::lock_guard<std::mutex> lk(head_mutex_);
+    if (head_ == get_tail()) {
+      return false;
+    }
+
+    Node* old_head = head_;
+    value = std::move(old_head->value);
+    head_ = old_head->next;
+    size_.fetch_sub(1, std::memory_order_release);
+    delete old_head;
+    return true;
+  }
+
+  T Dequeue() {
+    std::unique_lock<std::mutex> lk(head_mutex_);
+    cond_var_.wait(lk, [this] { return head_ != get_tail(); });
+
+    Node* old_head = head_;
+    T value = std::move(old_head->value);
+    head_ = old_head->next;
+    size_.fetch_sub(1, std::memory_order_release);
+    delete old_head;
+    return value;
+  }
+
+  bool empty() const {
+    std::unique_lock<std::mutex> lk(head_mutex_);
+    return head_ == get_tail();
+  }
+
+  size_t size() const { return size_.load(std::memory_order_acquire); }
+
+ private:
+  struct Node;
+
+  void InternalEnqueue() {
+    Node* new_tail = new Node;
+    tail_->next = new_tail;
+    tail_ = new_tail;
+    size_.fetch_add(1, std::memory_order_release);
+    cond_var_.notify_one();
+  }
+
+  Node* get_tail() const {
+    std::lock_guard<std::mutex> lk(tail_mutex_);
+    return tail_;
+  }
+
+  struct Node {
+    T value;
+    Node* next;
+
+    Node() {}
+    Node(const T& v) : value(v), next(nullptr) {}
+    Node(T&& v) : value(std::move(v)), next(nullptr) {}
+  };
+
+  mutable std::mutex head_mutex_;
+  mutable std::mutex tail_mutex_;
+  std::condition_variable cond_var_;
+  Node* head_;
+  Node* tail_;
+  std::atomic<size_t> size_;
+};
---
+++ test.cc
@@ -0,0 +1,75 @@
+#include "blocking_queue.h"
+
+#include <cassert>
+#include <chrono>
+#include <cstdio>
+#include <iostream>
+#include <thread>
+
+#include <cstdio>
+#define Log(...)                                                  \
+  fprintf(stderr, "[thread-%lu-%s]:", std::this_thread::get_id(), \
+          __FUNCTION__);                                          \
+  fprintf(stderr, __VA_ARGS__);                                   \
+  fprintf(stderr, "\n")
+
+const int kMaxElements = 1000000;
+
+int main(int argc, char const* argv[]) {
+  (void)argc;
+  (void)argv;
+
+  BlockingQueue<int> q;
+  std::atomic<bool> start(false);
+
+  std::thread t1([&] {
+    while (!start) {
+      std::this_thread::yield();
+    }
+    for (int i = 0; i < kMaxElements / 2; ++i) {
+      q.Enqueue(i);
+    }
+  });
+
+  std::thread t2([&] {
+    while (!start) {
+      std::this_thread::yield();
+    }
+    for (int i = 0; i < kMaxElements / 2; ++i) {
+      q.Enqueue(i);
+    }
+  });
+
+  std::thread t3([&] {
+    while (!start) {
+      std::this_thread::yield();
+    }
+    for (int i = 0; i < kMaxElements / 2; ++i) {
+      q.Dequeue();
+    }
+  });
+
+  std::thread t4([&] {
+    while (!start) {
+      std::this_thread::yield();
+    }
+    for (int i = 0; i < kMaxElements / 2; ++i) {
+      q.Dequeue();
+    }
+  });
+
+  start.store(true);
+  auto t1_ = std::chrono::steady_clock::now();
+  t1.join();
+  t2.join();
+  t3.join();
+  t4.join();
+  auto t2_ = std::chrono::steady_clock::now();
+
+  assert(q.size() == 0 && q.empty());
+  std::cout << "t2-t1="
+            << std::chrono::duration_cast<std::chrono::milliseconds>(t2_ - t1_)
+                   .count()
+            << "\n";
+  return 0;
+}
